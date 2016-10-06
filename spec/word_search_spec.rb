918d10fba49b43e982318c8897330eed1c3e5086
Sam Hopkins <hopkins.571@gmail.com>
2016-10-06 12:11:46 -0500

    improve rspec tests

--- spec/word_search_spec.rb
+++ spec/word_search_spec.rb
@@ -1,6 +1,34 @@
 require_relative '../word_search'
 
+RSpec::Matchers.define :be_boolean do
+  match do |actual|
+    expect(actual).to satisfy { |x| x == true || x == false }
+  end
+end
+
 describe 'straight_line_include?' do
+	let(:puzzle) { [
+		["a", "k", "f", "o", "x", "e", "s"], 
+		["s", "o", "a", "w", "a", "h", "p"], 
+		["i", "t", "c", "k", "e", "t", "n"],
+		["o", "t", "s", "d", "h", "o", "h"],
+		["s", "e", "x", "g", "s", "t", "a"],
+		["u", "r", "p", "i", "w", "e", "u"],
+		["z", "s", "b", "n", "u", "i", "r"]
+		] }
+
+	it 'returns a boolean' do
+		expect(straight_line_include?("anything", puzzle)).to be_boolean
+	end
+
+	it 'returns true if word found in straight line in puzzle' do
+		expect(straight_line_include?("foxes", puzzle)).to be true
+	end
+
+	it 'returns false if word isn\'t found in straight line in puzzle' do
+		expect(straight_line_include?("panda", puzzle)).to be false
+	end
+
 end
 
 describe 'snaking_include?' do
